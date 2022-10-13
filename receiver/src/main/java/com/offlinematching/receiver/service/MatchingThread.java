eadcd2f8553ab9f274a9bda09e430a6b900de9ad
raahaat <rahat_ahmed@ymail.com>
2022-10-13 12:40:12 +0600

    Matching remaining

---
+++ receiver/src/main/java/com/offlinematching/receiver/service/MatchingThread.java
@@ -0,0 +1,91 @@
+package com.offlinematching.receiver.service;
+
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import com.futronictech.AnsiSDKLib;
+
+public class MatchingThread implements Runnable{
+
+    Connection con;
+    Map<String, byte[]> customerFingers  = new HashMap<>();
+    int startIndex;
+    int chunk;
+    String customerNumber;
+    String token;
+
+    public MatchingThread() {
+    }
+
+    public MatchingThread(String token, String customerNumber,Connection con, Map<String, byte[]> customerFingers, int startIndex, int chunk) {
+        this.con = con;
+        this.customerFingers = customerFingers;
+        this.startIndex = startIndex;
+        this.chunk = chunk;
+        this.customerNumber = customerNumber;
+        this.token = token;
+    }
+
+    @Override
+    public void run() {
+        try {
+            fingerMatching();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void fingerMatching() throws SQLException
+    {
+        
+        List<Map> allFingerData = new ArrayList<>();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from fp_enroll order by cust_no"  + " OFFSET " + startIndex + " ROWS FETCH FIRST " + chunk + " ROWS ONLY");
+        while(rs.next())
+        {
+            Map<String, byte[]> singleFingerData = new HashMap<>();
+            singleFingerData.put("RTHUMB", getByteDataFromBlob(rs.getBlob("RTHUMB")));
+            singleFingerData.put("RINDEX", getByteDataFromBlob(rs.getBlob("RINDEX")));
+            singleFingerData.put("RMIDDLE", getByteDataFromBlob(rs.getBlob("RMIDDLE")));
+            singleFingerData.put("RRING", getByteDataFromBlob(rs.getBlob("RRING")));
+            singleFingerData.put("RLITTLE", getByteDataFromBlob(rs.getBlob("RLITTLE")));
+            singleFingerData.put("LTHUMB", getByteDataFromBlob(rs.getBlob("LTHUMB")));
+            singleFingerData.put("LINDEX", getByteDataFromBlob(rs.getBlob("LINDEX")));
+            singleFingerData.put("LMIDDLE", getByteDataFromBlob(rs.getBlob("LMIDDLE")));
+            singleFingerData.put("LRING", getByteDataFromBlob(rs.getBlob("LRING")));
+            singleFingerData.put("LLITTLE", getByteDataFromBlob(rs.getBlob("LLITTLE")));
+            allFingerData.add(singleFingerData);
+        }
+
+        for(int i = 0; i < allFingerData.size(); i++)
+        {
+            
+        }
+
+
+
+
+
+    }
+
+
+    public byte[] getByteDataFromBlob(Blob blob) {
+        if (blob != null) {
+            try {
+                return blob.getBytes(1, (int) blob.length());
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+        return null;
+    }
+
+}
